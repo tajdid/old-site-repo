0c7b25a02a1934a474cfa19b192950646461f85d
tajdid <tajdidrahman@gmail.com>
2015-01-16 17:16:07 +0000

    New version

---
+++ Gruntfile.js
@@ -0,0 +1,34 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    sass: {
+      dist: {
+        options: {
+          style: 'compressed'
+        },
+        files: {
+          'css/style.css': 'scss/style.scss'
+        }
+      }
+    },
+    watch: {
+      css: {
+        files: ['scss/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: true,
+        },
+      },
+
+      html: {
+        files: ['*.html'],
+        options: {
+          livereload: true,
+        },
+      }
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['sass', 'watch']);
+};
